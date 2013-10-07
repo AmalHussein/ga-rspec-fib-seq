d6b20ad11c12663fc6ea6a8f00eccde50bcefd26
Amal Hussein <amalhussein08@gmail.com>
2013-10-07 06:11:18 -0400

    Final commit--weird error though

--- lib/fiboseq.rb
+++ lib/fiboseq.rb
@@ -1,22 +1,23 @@
 require'pry'
 
-
 #Below is a method for calculating the Fibonacci Sequence into an Array 
 
 def fib(x)
-a = []
+  a = []
   x.times do |i|
     if i == 0
       a[i] = 0
     elsif i==1
       a[i] = 1
     else
-    a[i] = a[i-1] + a[i-2]
+      a[i] = a[i-1] + a[i-2]
     end  
   end
 
   return a
 end
+
+puts fib(8)
 
 #binding.pry 
 
--- spec/fibseq_spec.rb
+++ spec/fibseq_spec.rb
@@ -2,23 +2,32 @@ require 'spec_helper'
 require_relative '../lib/fiboseq'
 
 describe "#fib"  do 
- 	it "should have a method called fib" do
- 		expect(method(:fib))
- 	end 
 
- 	it "should put numbers into an incrementing array" do
- 		expect(fib(5)).to eq [0, 1, 1, 2, 3]
- 		expect(fib(2)).to eq [0,1]
- 		expect(fib(3)).to eq [0, 1, 1]
- 		expect(fib(4)).to eq [0, 1, 1, 2]
- 		expect(fib(10)).to eq [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+	it "should have a method called fib" do
+		expect(method(:fib))
+	end 
+
+	it "should have one parameter called x" do
+		parameters = method(:fib).parameters
+		expect(parameters[0]).to include(:req)
+		expect(parameters[0]).to include(:x)
+		expect(parameters.length).to eq 1
 	end
 
-it "show raise an error when a non-positive integer is passed as a parameter" do 
-    expect{ fib("this is text") }.to raise_error(NoMethodError)
-    expect{ fib(true) }.to raise_error(NoMethodError)
-    expect{ fib(false)}.to raise_error(NoMethodError)
-    expect{ fib([1,2]) }.to raise_error(NoMethodError)
-    expect{ fib(-1)}.to raise_error(NoMethodError)
-  end
 
+	it "should put numbers into an incrementing array" do
+		expect(fib(5)).to eq [0, 1, 1, 2, 3]
+		expect(fib(2)).to eq [0,1]
+		expect(fib(3)).to eq [0, 1, 1]
+		expect(fib(4)).to eq [0, 1, 1, 2]
+		expect(fib(10)).to eq [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+	end
+
+	it "show raise an error when a non-integer is passed as a parameter" do 
+		expect{ fib("this is text") }.to raise_error(NoMethodError)
+		expect{ fib(true) }.to raise_error(NoMethodError)
+		expect{ fib(false)}.to raise_error(NoMethodError)
+		expect{ fib([1,2]) }.to raise_error(NoMethodError)
+	end
+
+end 
